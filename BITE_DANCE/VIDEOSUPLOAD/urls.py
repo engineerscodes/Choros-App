06d68273bcd2212622ce76804db5e0214228dc9f
jayanpraveen <jayanpraveen30@gmail.com>
2021-04-05 13:37:06 +0530

    css

--- BITE_DANCE/VIDEOSUPLOAD/urls.py
+++ BITE_DANCE/VIDEOSUPLOAD/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload/',views.PUTVD,name="UPLOAD"),
-    path('videos/',views.allVideos,name="gallery"),
-    path('videos/<uuid>',views.getSingleVideo,name="Filter Video"),
-    path('',views.homePage,name="HOMEPAGE"),
-    path('bitdance/moderator/',views.Moderator,name="Moderator's"),
-    path('bitdance/GodMode/',views.GodMode,name="Total Marks")
+    path('upload/', views.PUTVD, name="UPLOAD"),
+    path('videos/', views.allVideos, name="gallery"),
+    path('videos/<uuid>', views.getSingleVideo, name="Filter Video"),
+    path('', views.homePage, name="HOMEPAGE"),
+    path('bitdance/moderator/', views.Moderator, name="Moderator's"),
+    path('bitdance/GodMode/', views.GodMode, name="Total Marks")
 
 ]

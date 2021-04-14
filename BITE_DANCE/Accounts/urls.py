b2664ae7dba23dec021bd526590ecbe11b222378
jayanpraveen <jayanpraveen30@gmail.com>
2021-04-14 21:02:42 +0530

    added change password

--- BITE_DANCE/Accounts/urls.py
+++ BITE_DANCE/Accounts/urls.py
@@ -5,21 +5,31 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # Account management
     path('login/', views.login, name="login"),
     path('reg/', views.reg, name="REG"),
     path('logout/', views.logout, name="LOGOUT"),
     path('activate/<uidb64>/<token>', views.AUTHUSERNAME, name="activate"),
 
-    path('password_reset/', auth_views.PasswordResetView.as_view(),
+    # Password management
+    # - forgot password
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="Password/password_reset.html"),
          name="password_reset"),
 
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="Password/password_reset_done.html"),
          name="password_reset_done"),
 
-    path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('password_reset_confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="Password/password_reset_confirm.html"), name="password_reset_confirm"),
 
-    path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete")
+    path('password_reset_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="Password/password_reset_complete.html"), name="password_reset_complete"),
+
+    # - change password
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name="Password/password_change.html"),
+         name="password_change"),
+
+    path('password_change/done/ ',
+         auth_views.PasswordChangeDoneView.as_view(), name='password_change_done')
 
 ]

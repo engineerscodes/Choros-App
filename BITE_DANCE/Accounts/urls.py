beb7c0c4f2d993d860fcfb234abfe32af54ca2a3
jayanpraveen <jayanpraveen30@gmail.com>
2021-04-14 19:24:06 +0530

    added forgot password

--- BITE_DANCE/Accounts/urls.py
+++ BITE_DANCE/Accounts/urls.py
@@ -1,10 +1,25 @@
+from os import name
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/',views.login,name="login"),
-    path('reg/',views.reg,name="REG"),
-    path('logout/',views.logout,name="LOGOUT"),
-    path('activate/<uidb64>/<token>',views.AUTHUSERNAME,name="activate")
+    path('login/', views.login, name="login"),
+    path('reg/', views.reg, name="REG"),
+    path('logout/', views.logout, name="LOGOUT"),
+    path('activate/<uidb64>/<token>', views.AUTHUSERNAME, name="activate"),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(),
+         name="password_reset"),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+         name="password_reset_done"),
+
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete")
+
 ]
--- BITE_DANCE/BITE_DANCE/urls.py
+++ BITE_DANCE/BITE_DANCE/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/',include('Accounts.urls')),
-    path('',include('VIDEOSUPLOAD.urls'))
+    path('account/', include('Accounts.urls')),
+    path('', include('VIDEOSUPLOAD.urls'))
 ]
 
-urlpatterns= urlpatterns +static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
